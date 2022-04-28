7ba94fd217d8a0af772aacc87b06fc96218d25c5
barefoot-yeti <groenie007@gmail.com>
2022-04-28 17:19:12 +0200

    added comments and wired them up

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_27_170349) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_28_145346) do
+  create_table "comments", force: :cascade do |t|
+    t.integer "post_id", null: false
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
+
   create_table "models", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -43,4 +51,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_27_170349) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "posts"
 end
