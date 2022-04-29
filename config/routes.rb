3c9f914463c869b5b3640824b28a3e544c833e9e
barefoot-yeti <groenie007@gmail.com>
2022-04-29 19:39:45 +0200

    added home,contact and about page and wired them up with a controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
+root "posts#index"
   devise_for :users
+  resources :home
+  resources :contact
+  resources :about
   resources :posts do
     resources :comments
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-root "posts#index"
 end
