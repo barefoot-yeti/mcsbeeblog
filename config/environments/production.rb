50d9e9641b67518b4fa6aac02fdd9e197beefcdc
barefoot-yeti <groenie007@gmail.com>
2022-05-02 19:17:59 +0200

    Try again with prod enviroment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   #devise stuff
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { :host => 'mcsbeeblog.heroku.com'
   
   #letter opener for dev enviromoment
   config.action_mailer.perform_deliveries = false
